00f6a15d2a8b8e77b06534e7bcca9c15ef14b23d
smallfoot <smallfoot@osiris.cyber.nyu.edu>
2026-08-25 14:22:32 -0400

    Init (Case 1 done)

---
+++ 1. hello world/hello_world.rs
@@ -0,0 +1,15 @@
+fn main() {
+    println!("{}", "Hello World");
+    // macro!(...) => extends rust syntax
+    //function(...) => good ol' function
+}
+
+/*
+    NOTE:
+    rustfmt => automatic formatter tool 
+ */
+
+/*
+    compile with:
+        rustc [file].rs => generates an executible
+*/
